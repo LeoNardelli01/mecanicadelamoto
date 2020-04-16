3765c113284743b7272b30ba0273206890bf5fbe
Leonel Nardelli <leonardelli@MacBook-Pro-de-Leonel.local>
2020-04-16 18:52:51 -0300

    link con ws

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 $(function(){
+  var whatsapp = "https://api.whatsapp.com/send?phone=5491136393404&text=Por%20favor%20quiero%20informaci%C3%B3n%20de%20los%20cursos%20de%20mecanica"
+
+  $(".btn-whatsapp").click(function(){
+    $(location).attr('href',whatsapp);
+  });
+
   
 });
