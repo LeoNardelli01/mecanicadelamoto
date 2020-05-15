65779a81ccebc8ebced258dd5d86428042e2435f
Leonel Nardelli <leonardelli@MacBook-Pro-de-Leonel.local>
2020-05-15 00:02:21 -0300

    google anallitycs agregado

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(function(){
-  var whatsapp = "https://api.whatsapp.com/send?phone=5491127161785&text=Por%20favor%20quiero%20informaci%C3%B3n%20de%20los%20cursos%20de%20mecanica";
+  var whatsapp = "https://api.whatsapp.com/send?phone=5491136393404&text=Por%20favor%20quiero%20informaci%C3%B3n%20de%20los%20cursos%20de%20mecanica";
   var messenger = "https://www.messenger.com/login.php?next=https%3A%2F%2Fwww.messenger.com%2Ft%2F1245073048897844%2F%3Fmessaging_source%3Dsource%253Apages%253Amessage_shortlink";
   var telefono = "tel:+5491136393404";
   var facebook = "https://www.facebook.com/MecanicaOficial";
